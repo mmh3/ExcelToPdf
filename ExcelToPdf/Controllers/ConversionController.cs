4ac6d0de5df31851584326562de105d60d600caa
mmh <matt.hensinger@gmail.com>
2021-09-26 22:48:00 -0400

    Change to use tildes instead of dashes in string replacement for file name in URL.

--- ExcelToPdf/Controllers/ConversionController.cs
+++ ExcelToPdf/Controllers/ConversionController.cs
@@ -14,10 +14,10 @@ namespace ExcelToPdf.Controllers
         [ActionName("Convert")]
         public IHttpActionResult Convert(string id)
         {
-            // There are issues with sending in special characters in the path (:, /, .).
-            // Convert -- => :/ and - => /. Also, assume any file passed in is a ".xlsx" file type.
-            var filename = id.Replace("--", ":/");
-            filename = filename.Replace("-", "/");
+            // There are issues with sending in special characters in the path (:, \, .).
+            // Convert -- => :\ and - => \. Also, assume any file passed in is a ".xlsx" file type.
+            var filename = id.Replace("~~", @":\");
+            filename = filename.Replace("~", @"\");
             filename += ".xlsx";
 
             var excelInteropExcelToPdfConverter = new ExcelInteropExcelToPdfConverter();
